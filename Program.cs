4d1c7ed71d21ae63b0d095a3205a129cc4a7858e
student <zofia.glowacka@uczen.zsk.poznan.pl>
2024-11-19 07:44:18 +0100

    add base code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace star_battle_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int board_size = 9;
+            
+        }
+    }
+}
+public class Cell
+{
+    public string State { get; set; }
+    public int Group { get; set; }
+}
