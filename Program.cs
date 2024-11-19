a3cbb9a4a4f0c8b867af7a99623d4ac2ca5e675b
student <zofia.glowacka@uczen.zsk.poznan.pl>
2024-11-19 07:55:35 +0100

    add cell and board class

--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@
     {
         static void Main(string[] args)
         {
-            int board_size = 9;
             
+
         }
     }
 }
 public class Cell
 {
-    public string State { get; set; }
+    public string State { get; set; } = " ";
     public int Group { get; set; }
+}
+public class Board
+{
+    public static int board_size = 9;
+    public Cell[,] game_board = new Cell[board_size, board_size];
+    // tutaj dac zczytywanie z pliku grup
 }
