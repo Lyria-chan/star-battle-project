c6a642bd2aac77351af55d1111701758e6cafad3
student <zofia.glowacka@uczen.zsk.poznan.pl>
2024-11-26 08:44:05 +0100

    Add import and some basic functions (FOR REVIEW)

--- Program.cs
+++ Program.cs
@@ -1,22 +1,76 @@
-﻿namespace star_battle_project
+﻿
+using System.Text.Json;
+
+namespace star_battle_project
 {
-    internal class Program
+    using System.Text.Json;
+
+    // Star Battle in CMD
+    class StarBattle
     {
+        static int GameSize; // Dynamically set based on the imported board
+        static List<Cell> GameBoard = new List<Cell>();
+
+        public class Cell
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Group { get; set; }
+            public char State { get; set; }
+
+            public Cell(int x, int y, int group, char state)
+            {
+                X = x;
+                Y = y;
+                Group = group;
+                State = state;
+            }
+        }
+
+        static void PrintBoard()
+        {
+            foreach (var cell in GameBoard)
+            {
+
+                Console.Write(cell.State);
+                if (cell.Y == GameSize - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static List<Cell> ImportBoard(string boardFile)
+        {
+            var board = new List<Cell>();
+
+
+            string json = File.ReadAllText(boardFile);
+            var groups = JsonSerializer.Deserialize<Dictionary<string, List<List<int>>>>(json)["game_board"];
+
+            // Determine GameSize from the size of the board
+            GameSize = groups.Count;
+
+            for (int rowNumber = 0; rowNumber < groups.Count; rowNumber++)
+            {
+                var row = new List<Cell>();
+                for (int yNumber = 0; yNumber < groups[rowNumber].Count; yNumber++)
+                {
+                    int cellGroup = groups[rowNumber][yNumber];
+                    board.Add(new Cell(rowNumber, yNumber, cellGroup, 'x'));
+                }
+            }
+
+            return board;
+        }
+
         static void Main(string[] args)
         {
-            
-
+            GameBoard = ImportBoard("game_board.json");
+            Console.WriteLine($"Game Size: {GameSize}x{GameSize}");
+            PrintBoard();
+            Console.ReadLine();
         }
+
     }
-}
-public class Cell
-{
-    public string State { get; set; } = " ";
-    public int Group { get; set; }
-}
-public class Board
-{
-    public static int board_size = 9;
-    public Cell[,] game_board = new Cell[board_size, board_size];
-    // tutaj dac zczytywanie z pliku grup
 }
